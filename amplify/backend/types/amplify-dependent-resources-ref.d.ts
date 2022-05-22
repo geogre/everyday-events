0482fa08b9a52c8ce74376ee8bdca2adc2d54001
yurii.vakulik <bestgeogre@gmail.com>
2022-05-22 20:36:00 +0300

    update version and add a new file

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "everydayevents47b8cd8a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "storage": {
+        "eebucket": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
