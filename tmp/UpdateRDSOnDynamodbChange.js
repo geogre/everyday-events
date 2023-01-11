b7d11bae1eb957b88e3798be82b4aad0e773dff7
yurii.vakulik <bestgeogre@gmail.com>
2023-01-11 19:48:25 +0200

    add UpdateRDSOnDynamodbChange.js

---
+++ tmp/UpdateRDSOnDynamodbChange.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+// Set the region
+const region = "us-east-1";
+const secretName = "prod/eevents/mysql";
+
+AWS.config.update({region: region});
+
+
+const getSecret = async () => {
+	// Create a Secrets Manager client
+	const client = new AWS.SecretsManager({
+		region: region
+	});
+	const data = await client.getSecretValue({SecretId: secretName}).promise();
+	return JSON.parse(data.SecretString);
+}
+
+const mysql = require('mysql2/promise');
+
+
+exports.handler = async (event, context) => {
+
+
+	const secret = await getSecret();
+	const connection = await mysql.createConnection({
+		host     : secret.host,
+		user     : secret.username,
+		password : secret.password,
+		port     : secret.port
+	});
+
+	for (const record of event.Records) {
+		if (record.eventName === 'INSERT') {
+			const item = record.dynamodb.NewImage;
+			await connection.execute("INSERT INTO everydayeventsdb.eevents (`userId`, `eventId`, `title`, `eventDate`) VALUES (?, ?, ?, ?)", [item.userId.S, item.eventId.S, item.title.S, item.eventDate.S]);
+		} else if (record.eventName === 'MODIFY') {
+			const item = record.dynamodb.NewImage;
+			const keys = record.dynamodb.Keys;
+			await connection.execute("UPDATE everydayeventsdb.eevents SET title = ?, eventDate = ? WHERE userId = ? AND eventId = ?", [item.title.S, item.eventDate.S, keys.userId.S, keys.eventId.S]);
+		} else if (record.eventName === 'REMOVE') {
+			const keys = record.dynamodb.Keys;
+			await connection.execute("DELETE FROM everydayeventsdb.eevents WHERE userId = ? AND eventId = ?", [keys.userId.S, keys.eventId.S]);
+		}
+		console.log(record.eventID);
+		console.log(record.eventName);
+		console.log('DynamoDB Record: %j', record.dynamodb);
+	}
+	return `Successfully processed ${event.Records.length} records.`;
+};
